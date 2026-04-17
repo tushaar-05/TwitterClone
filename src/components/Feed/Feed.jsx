85b621958f9cde89ce3836c6a5397207db8eaa81
Tushar R Singh <tusharsingh222555@gmail.com>
2026-04-18 01:41:09 +0530

    TopFeed Bar

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import PostCard from "../cards/Post"
+import FeedTop from "./FeedTop"
 
 const Feed = () => {
+
+
   return (
-    <div className=''>
-      <div className="sticky top-0 w-full h-14 z-10">
-        <div className='flex h-full w-full'>
-          <button className='h-full w-1/2 hover:bg-white/10 cursor-pointer'></button>
-          <button className='h-full w-1/2 hover:bg-white/10 cursor-pointer'></button>
-        </div>
-      </div>
+    <div>
+      <FeedTop />
+      
       <div>
         <PostCard />
         <PostCard />
