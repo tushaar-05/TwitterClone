6efa04ecb13f3d1ee4e9fb0b28414f0e2a0320c9
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-04-17 23:33:34 +0530

    ui fixes

--- src/components/RightPanel/News.jsx
+++ src/components/RightPanel/News.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const News = () => {
   return (
-    <div className="w-full h-1/3">
-      <div className="w-full h-5/11 text-base font-bold leading-5.5 mb-1">
+    <div className="w-full h-1/3 mb-2">
+      <div className="w-full h-5/11 text-sm font-extrabold leading-5.5">
         Shreyas Iyer Praises RCB Captain Rajat Patidar in IPL Chat
       </div>
       <div className="w-full h-1/3 flex items-center">
--- src/components/RightPanel/RightPanel.jsx
+++ src/components/RightPanel/RightPanel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import TodaysNews from './TodaysNews'
+import Searchbar from './Searchbar'
 
 const RightPanel = () => {
   return (
     <div className='pt-10'>
+      <Searchbar/>
       <TodaysNews/>
     </div>
   )
