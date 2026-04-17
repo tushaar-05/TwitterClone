abc3f2fa46b7f260a8e0e64395cb3b13ab56cdcc
Tushar R Singh <tusharsingh222555@gmail.com>
2026-04-18 01:13:06 +0530

    SAVE PROGRESS

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -3,12 +3,21 @@ import PostCard from "../cards/Post"
 
 const Feed = () => {
   return (
-    <>
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-    </>
+    <div className=''>
+      <div className="sticky top-0 w-full h-14 z-10">
+        <div className='flex h-full w-full'>
+          <button className='h-full w-1/2 hover:bg-white/10 cursor-pointer'></button>
+          <button className='h-full w-1/2 hover:bg-white/10 cursor-pointer'></button>
+        </div>
+      </div>
+      <div>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
+      
+    </div>
   )
 }
 
