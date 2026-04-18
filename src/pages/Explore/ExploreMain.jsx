9de51558c5994b6160d2d4df1c89622a86cbdc52
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-04-18 11:16:44 +0530

    Explore Page Initial setup and Search bar ui component

---
+++ src/pages/Explore/ExploreMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExploreSearchbar from './ExploreSearchbar'
+
+const ExploreMain = () => {
+  return (
+    <div className='pt-1 px-4'>
+        <ExploreSearchbar/>
+    </div>
+  )
+}
+
+export default ExploreMain
---
+++ src/pages/Explore/ExploreSearchbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Search,Settings } from "lucide-react";
+
+const ExploreSearchbar = () => {
+  return (
+    <div className="flex items-center justify-between w-full h-11.5">
+      <div className="w-6/7 h-full rounded-full border-1 border-[#2f3336] flex gap-2 focus-within:border-blue-50">
+        <Search className="mt-3 ml-2.5 text-[#71767b] w-5 h-5" />
+        <input className="w-full outline-none" placeholder="Search" />
+      </div>
+      <Settings className="w-5 h-5"/>
+    </div>
+  );
+};
+
+export default ExploreSearchbar;
