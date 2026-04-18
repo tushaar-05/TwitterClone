56e52e7efc5b87d73dbce7652b82c7600ac84bd3
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-04-18 12:36:52 +0530

    Explore Page Today's post

--- src/pages/Explore/ExploreMain.jsx
+++ src/pages/Explore/ExploreMain.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ExploreSearchbar from './ExploreSearchbar'
 import ExplorePageCategoryTab from './ExplorePageCategoryTab'
+import ExploreTodaysNews from './ExploreTodaysNews'
 
 const ExploreMain = () => {
   return (
     <div className='pt-1'>
         <ExploreSearchbar/>
         <ExplorePageCategoryTab/>
+        <ExploreTodaysNews/>
     </div>
   )
 }
