19c0ae443a5cd9233f88079642d665ce87ca0166
Divyam-Nailwal <nailwaldivyam@gmail.com>
2026-04-18 16:13:46 +0530

    Profile Top Card done

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import Profile from "./pages/Profile"
+
+
 createRoot(document.getElementById('root')).render(
-  // <BrowserRouter>
+  <BrowserRouter>
     <Profile />
-  // </BrowserRouter>
+  </BrowserRouter>
 )
